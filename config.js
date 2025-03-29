bedd36947792151b10dcd5eafa89d2ed278579cc
RAHUL-MASTER <rahulhiran37@gmail.com>
2025-03-29 10:04:59 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 //---------------------------------------------------------------------------
-//           EMPIRE-MD  
+//           RAHUL-AI-V3  
 //---------------------------------------------------------------------------
-//  @project_name : EMPIRE-MD  
+//  @project_name : RAHUL-AI-V3  
 //  @author       : efeurhobobullish
 //  ⚠️ DO NOT MODIFY THIS FILE ⚠️  
 //---------------------------------------------------------------------------
@@ -15,11 +15,11 @@ function convertToBool(text, fault = 'true') {
 //═════[Don't Change Variables]════════\\
 
 global.alive_img = "📞 𝙰𝚞𝚝𝚘 𝙲𝚊𝚕𝚕 𝚁𝚎𝚓𝚎𝚌𝚝 𝙼𝚘𝚍𝚎 𝙰𝚌𝚝𝚒𝚟𝚎.📵 𝙽𝚘 𝙲𝚊𝚕𝚕𝚜 𝙰𝚕𝚕𝚘𝚠𝚎𝚍!";
-global.caption = "𝙴𝙼𝙿𝙸𝚁𝙴-𝙼𝙳 𝙿𝚘𝚠𝚎𝚛𝚎𝚍 𝚋𝚢 𝙴𝚖𝚙𝚒𝚛𝚎 𝚃𝚎𝚌𝚑";
-global.channelUrl = "https://whatsapp.com/channel/0029VajVvpQIyPtUbYt3Oz0k";
-global.botname = "𝙴𝙼𝙿𝙸𝚁𝙴-𝙼𝙳";
-global.devs = "2348078582627" // Developer Contact
-global.devsname = "𝙴𝚖𝚙𝚒𝚛𝚎 𝚃𝚎𝚌𝚑";
+global.caption = "RAHUL-AI-V3 𝙿𝚘𝚠𝚎𝚛𝚎𝚍 𝚋𝚢 𝙴𝚖𝚙𝚒𝚛𝚎 𝚃𝚎𝚌𝚑";
+global.channelUrl = "";
+global.botname = "RAHUL-AI-V3";
+global.devs = "919356730236" // Developer Contact
+global.devsname = "Rahul Master";
 global.apikey = "_0x5aff35,_0x1876stqr";
 
 module.exports = {
@@ -34,9 +34,9 @@ module.exports = {
     AUTO_RECORDING: process.env.AUTO_RECORDING || "false",
     AUTO_TYPING: process.env.AUTO_TYPING || "false",
     AUTO_VIEW_STATUS: process.env.AUTO_VIEW_STATUS || "false",
-    MODE: process.env.MODE || "private",
-    OWNER_NAME: process.env.OWNER_NAME || "𝙴𝚖𝚙𝚒𝚛𝚎 𝚃𝚎𝚌𝚑",
-    OWNER_NUMBER: process.env.OWNER_NUMBER || "2348144250768",
+    MODE: process.env.MODE || "public",
+    OWNER_NAME: process.env.OWNER_NAME || "Rahul Master",
+    OWNER_NUMBER: process.env.OWNER_NUMBER || "919356730236",
     PREFIX: process.env.PREFIX || ".",
     SESSION_ID: process.env.SESSION_ID || "zRJ2zQya#Ez8XGgRV_CD7Y6xCZnoc0pcEtlR6e_BIHbcydWQ0CfQ"
 };
