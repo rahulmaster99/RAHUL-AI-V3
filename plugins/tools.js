f0c855659d63d7d746a6c9955269b46d49c3c0e7
RAHUL-MASTER <rahulhiran37@gmail.com>
2025-03-30 11:05:57 +0530

    Update tools.js

--- plugins/tools.js
+++ plugins/tools.js
@@ -1,6 +1,6 @@
 const config = require('../config');
 const { cmd, commands } = require('../command');
-const { sleep } = require('../Lib/functions');
+const { sleep } = require('../lib/functions');
 
 // Shutdown command
 cmd({
