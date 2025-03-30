e8cc5dee1f548b4e6ffbe8b0dd5528375b2f4235
RAHUL-MASTER <rahulhiran37@gmail.com>
2025-03-30 11:28:03 +0530

    Update group.js

--- plugins/group.js
+++ plugins/group.js
@@ -9,7 +9,7 @@ const { cmd, commands } = require('../command');
 const config = require('../config');
 const prefix = config.PREFIX;
 const fs = require('fs');
-const { getBuffer, getGroupAdmins, getRandom, h2k, isUrl, Json, sleep, fetchJson } = require('../Lib/functions');
+const { getBuffer, getGroupAdmins, getRandom, h2k, isUrl, Json, sleep, fetchJson } = require('../lib/functions');
 const { writeFileSync } = require('fs');
 const path = require('path');
 
