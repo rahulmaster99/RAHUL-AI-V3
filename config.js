5809be681d4186ac45fa836dad47d75a6d0653c5
RAHUL-MASTER <rahulhiran37@gmail.com>
2025-03-30 23:53:12 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,11 +5,11 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "EC1l1RKZ#iIk20jIp2r7-3tmLs0h2qfqTCnPfenAgsis_CVXc-3w",
-ALIVE_IMG: process.env.ALIVE_IMG || "https://i.imgur.com/HO8U7W1.jpeg",
-SUDO_NB: process.env.SUDO_NB || "2349073081822",
+SESSION_ID: process.env.SESSION_ID || "RAHUL-AI-MD~fUw1GTQT#ek_TUTma_R4Xgy1sfiD0qCP3dgpCAZpZAGBgnxuBd1E",
+ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/bruryh.jpeg",
+SUDO_NB: process.env.SUDO_NB || "919356730236",
 MONGODB: process.env.MONGODB || "mongodb+srv://kulathungaasitha319:yjHB0DvFfStNfwPS@cluster0.3oijd.mongodb.net/",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
-MODE: process.env.MODE || "private",
-AUTO_VOICE: "true"
+MODE: process.env.MODE || "public",
+AUTO_VOICE: "false"
 };
